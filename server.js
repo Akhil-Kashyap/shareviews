2daafd3eb083fe243898943f4040d6642307ae50
Akhil Kashyap <Akhil-Kashyap@users.noreply.github.com>
2020-07-26 18:28:47 +0530

    For Deploy

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use("/posts", posts);
 
 //server static assets if in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
